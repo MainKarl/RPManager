5a9189f298d01b08c4ceaed8d805fabe4cb6ceb8
Karl Mainville <karlmainville26@gmail.com>
2022-11-23 15:00:53 -0500

    Adding the refresh the armor list after adding

---
+++ Isekai/IsekaiWeb/wwwroot/js/showAlert.js
@@ -0,0 +1,9 @@
+﻿function showAlert(name) {
+    $.alert({
+        title: 'Armor Added!',
+        content: name + ' was added to the armor list!',
+        confirm: function () {
+            refreshArmorList(1)
+        }
+    })
+}
